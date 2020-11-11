ed9af3b34f09d598e9f86c135d414b7e7f5fa163
suheel11 <vallamkonda.s@northeastern.edu>
2020-11-11 17:52:38 -0500

    cloudwatch

--- codedeploy_scripts/applicationStart.sh
+++ codedeploy_scripts/applicationStart.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/cloudwatch-config.json -s
 
 cd /home/ubuntu/target
 java -jar user-0.0.1-SNAPSHOT.jar
+
